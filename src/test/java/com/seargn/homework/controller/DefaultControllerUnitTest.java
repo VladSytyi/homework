cf8c200bebfd7d3b557d3a52397a7096ae7b57b6
Vladislav Sytyi <valdis3gs@gmail.com>
2024-07-24 17:58:40 +0200

    feat: renaming

--- src/test/java/com/seargn/homework/controller/DefaultControllerUnitTest.java
+++ src/test/java/com/seargn/homework/controller/DefaultControllerUnitTest.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-public class DefaultControllerTest {
+public class DefaultControllerUnitTest {
 
     @Mock
     private EmployeeService employeeService;
